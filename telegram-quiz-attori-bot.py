e549811a92c28da592baed3b071e4b9bbe7fcbad
Vullnet Vogli <vuvogli@gmailo.com>
2021-01-13 20:26:07 +0100

    ok

--- telegram-quiz-attori-bot.py
+++ telegram-quiz-attori-bot.py
@@ -107,6 +107,6 @@ def start():
     bot.polling(none_stop=True, interval=0.5)
 
 if __name__ == '__main__':
-
+    print('ok')
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
     
