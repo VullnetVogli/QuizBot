f4909f94ee3ee0dc434eee7ffcb9e4dacd4d8faa
Vullnet Vogli <vuvogli@gmailo.com>
2021-01-13 16:41:05 +0100

    Ciao

--- telegram-quiz-attori-bot.py
+++ telegram-quiz-attori-bot.py
@@ -5,24 +5,35 @@ import os
 from flask import Flask, request
 
 TOKEN = '1544405331:AAGeXsgA384zMu3Xfpd-y4nkUFZ0LR4XGeY'
-URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+URL = 'https://telegram-quiz-attori-bot.herokuapp.com/' + TOKEN
 PATH = os.path.join(os.getcwd(), 'Immagini')
 
 bot = telebot.TeleBot(token = TOKEN, threaded = False)
+bot.remove_webhook()
+bot.set_webhook(url = URL)
 
 app = Flask(__name__)
 
 @app.route('/' + TOKEN, methods = ['POST'])
 def webhook():
     
-    bot.remove_webhook()
-    
-    bot.set_webhook(url = URL)
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+
+    bot.process_new_updates(updates = [update])
 
     return 'ok', 200
 
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands = ['start', 'help'])
 def start(message):
 
     bot.send_message(text = 'Benvenuto\n In questo quiz cercherai di indovinare gli attori nelle immagini!', chat_id = message.chat.id)
 
+
+@bot.message_handler(commands = ['contact'])
+def start(message):
+
+    bot.send_message(text = 'occo', chat_id = message.chat.id)
+
+if __name__ == '__main__':
+    
+    app.run(host = '0.0.0.0', port = int(os.environ.get('PORT', 5000)))
