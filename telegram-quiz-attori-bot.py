e99704ca6434411c865255e6cdd4df8a6f1c2a4e
Vullnet <vuvogli@gmail.com>
2021-01-17 16:45:18 +0100

    Gli attori indovinati non compariranno più
    Signed-off-by: Vullnet <vuvogli@gmail.com>

--- telegram-quiz-attori-bot.py
+++ telegram-quiz-attori-bot.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
 import telebot
 from mysql import connector
 from random import randint
 from datetime import datetime
 import os
+from random import randint
 
 
 IP = '80.210.122.173'
@@ -16,14 +16,17 @@ DATABASE = '5AI.VOGLI'
 TOKEN = '1544405331:AAGeXsgA384zMu3Xfpd-y4nkUFZ0LR4XGeY'
 URL = 'https://telegram-quiz-attori.herokuapp.com/' + TOKEN
 
-#server = Flask(__name__)
-
 bot = telebot.TeleBot(token = TOKEN, threaded = False)
-bot.remove_webhook()
 
 connessione = connector.connect(host = IP, port = PORTA, user = USER, password = PASSWORD, database = DATABASE)
 
 cursore = connessione.cursor()
+
+cursore.execute('select count(*) from quiz')
+
+dimensione = cursore.fetchone()[0]
+    
+ids = [i + 1 for i in range(dimensione)]
 
 idQuiz = -1
 nome = ''
@@ -35,21 +38,28 @@ def help(message):
     bot.send_message(message.chat.id, 'Scrivi /quiz per ricevere una nuova immagine da indovinare.\nScrivi /hint per un indizio')
 
 @bot.message_handler(commands = ['quiz'])
-def quizz(message):
+def quiz(message):
 
-    global idQuiz, nome, aiuto
+    global ids, idQuiz, nome, aiuto
 
-    cursore.execute('select count(*) from quiz')
+    if len(ids) == 0:
 
-    dimensione = cursore.fetchone()[0]
-    
-    cursore.execute('select * from quiz where idQuiz = {}'.format(randint(1, dimensione)))
-    
-    idQuiz, nome, aiuto = cursore.fetchone()
+        bot.send_message(text = 'Hai indovinato tutti i nomi degli attori, complimenti mi hai battuto! Per rigiocare digita /quiz.', chat_id = message.chat.id)
 
-    print(idQuiz, nome, aiuto)
+        idQuiz = -1
 
-    bot.send_photo(photo = open('Immagini//{}.jpg'.format(idQuiz), 'rb'), caption = 'Chi è?', chat_id = message.chat.id)
+        ids = [i + 1 for i in range(dimensione)]
+
+    else:
+
+        # Prendo un indice dal random, lo salvo e lo rimuovo per poi effettuare la query e prendere i dati
+        i = randint(0, len(ids) - 1)
+        
+        cursore.execute('select * from quiz where idQuiz = {}'.format(ids[i]))
+        
+        idQuiz, nome, aiuto = cursore.fetchone()
+        
+        bot.send_photo(photo = open('Immagini//{}.jpg'.format(idQuiz), 'rb'), caption = 'Chi è?', chat_id = message.chat.id)
 
 @bot.message_handler(commands = ['hint'])
 def hint(message):
@@ -65,12 +75,23 @@ def hint(message):
 
 @bot.message_handler(content_types = ['text'])
 def chat_message(message):
-    
+ 
+    global ids, idQuiz
+
     if idQuiz != -1:
 
         if nome in message.text.lower():
 
-            bot.send_message(text = 'Bravo! /quiz', chat_id = message.chat.id)
+            if len(ids):
+
+                bot.send_message(text = 'Bravo! /quiz', chat_id = message.chat.id)
+            
+                ids.remove(idQuiz)
+
+            else:
+
+                bot.send_message(text = 'Hai indovinato tutti i nomi degli attori, complimenti mi hai battuto!', chat_id = message.chat.id)
+
 
         else:
 
@@ -85,23 +106,7 @@ def chat_message(message):
     cursore.execute('insert into risposta values(null, "{}", "{}", "{}")'.format(data, ora, message.text))
 
     connessione.commit()
-'''
-@server.route('/{}'.format(TOKEN), methods=['POST'])
-def getMessage():
 
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    
-    bot.remove_webhook()
-
-    bot.set_webhook(url = URL)
-    
-    return "!", 200
-'''
 def start():
 
     bot.polling(none_stop = True, interval = 0.5)
